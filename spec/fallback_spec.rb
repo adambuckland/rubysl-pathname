845d5f9d5eef81935f0d30aeabb321c0769d132b
Benny <r3qnbenni@gmail.com>
2014-09-23 21:01:57 +0200

    don't pollute the namespace with fixture classes - use mock objects

--- spec/fallback_spec.rb
+++ spec/fallback_spec.rb
@@ -1,24 +1,18 @@
 require 'pathname'
 
-class RootPath
-  def to_str
-    '/'
-  end
-end
-
-class RootPath2
-  def to_path
-    '/'
-  end
-end
-
 describe "Pathname.new" do
   it "returns a new Pathname Object which uses to_str impl of the param" do
-    Pathname.new(RootPath.new).should == Pathname.new('/')
+    obj = mock("to_str")
+    obj.should_receive(:to_str).and_return("/")
+
+    Pathname.new(obj).should == Pathname.new('/')
   end
 
   it "returns a new Pathname Object which uses to_path impl of the param" do
-    Pathname.new(RootPath2.new).should == Pathname.new('/')
+    obj = mock("to_path")
+    obj.should_receive(:to_path).and_return("/")
+
+    Pathname.new(obj).should == Pathname.new('/')
   end
 end
 
